169a9aac783c36cefcf850db3103d36a95064cd4
nazariiko <nazarii.dev@gmail.com>
2023-11-03 22:19:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import ChatGPTService from './services/chatgpt.js';
+import { getSearchResult, parseAmazonProducts } from './services/google.js';
+import { getProducts } from './services/amazon.js';
+
+dotenv.config()
+
+const app = express();
+app.use(cors({
+  origin: '*'
+}));
+
+const PORT = 3000 || process.env.PORT
+const chatgptServiceInstance = new ChatGPTService();
+
+
+app.get('/search', async (req, res) => {
+  const dateStart = new Date()
+
+  const text = req.query.text;
+  const searchTerm = await chatgptServiceInstance.getSimpleSearchTerm(text);
+
+  let productResults;
+
+  const { isProduct, link } = await getSearchResult(searchTerm);
+  if (isProduct) {
+    console.log(link);
+  } else {
+    productResults = await parseAmazonProducts(link)
+  }
+
+  // productResults = await getProducts(searchTerm)
+
+  const bestMatchesProductsIndexes = await chatgptServiceInstance.getBestMatchesProducts(text, productResults);
+  const finalProducts = [];
+  bestMatchesProductsIndexes.forEach(index => {
+    finalProducts.push(productResults[+index - 1])
+  })
+
+  const top3Products = finalProducts.slice(0, 3)
+
+  const dateEnd = new Date()
+  const time = dateEnd - dateStart;
+  console.log(time / 1000);
+  res.json(top3Products)
+})
+
+
+app.listen(PORT, () => {
+  console.log('Server runs');
+})
---
+++ services/chatgpt.js
@@ -0,0 +1,50 @@
+import OpenAI from 'openai';
+
+export default class ChatGPTService {
+  constructor() {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    this.openai = openai
+  }
+
+  getSimpleSearchTerm = (text) => {
+    return new Promise(async (resolve, reject) => {
+      const completion = await this.openai.chat.completions.create({
+        messages: [{ role: "system", content: `Provide a simple product search term for google: "${text}"? Don't write any other text.` }],
+        model: "gpt-4",
+      });
+      const result = completion.choices[0]['message']['content'].replaceAll('"', '')
+      resolve(result)
+    })
+  }
+
+  getBestMatchesProducts = (search, products) => {
+    return new Promise(async (resolve, reject) => {
+      const listProduct = products.map((product, index) => {
+        return `${index + 1}. ${product.title}. ${product.price}.`
+      })
+      const completion = await this.openai.chat.completions.create({
+        messages: [
+          { 
+            role: "system", 
+            content: `
+              which of the following list products most closely matches the description "${search}"?
+              arrange in order from most suitable. at least 3 products, but more is possible.
+              write only the numbers on separate lines. do not write any words or sentences . do not write "none of this items match the description".
+              prices are listed at the end, if necessary:
+              ${listProduct.join(' ')}
+            ` 
+          }
+        ],
+        model: "gpt-4",
+      });
+      const result = completion.choices[0]['message']['content']
+      const resultArr = result.split('\n')
+      resolve(resultArr)
+    })
+  }
+}
+
+
