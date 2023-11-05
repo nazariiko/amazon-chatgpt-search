37e1074185131dc0cae4cf1a6982bf77711f6cda
nazariiko <nazarii.dev@gmail.com>
2023-11-05 17:40:45 +0200

    fix

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ app.get('/search', async (req, res) => {
     const text = req.query.text;
     const searchTerm = await chatgptServiceInstance.getSimpleSearchTerm(text);
 
-    console.log('chatgpt-1');
-
     let productResults;
 
     const link = await getSearchResult(searchTerm);
