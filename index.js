ae5aa0d53d3e881e2c01abce1ac362c8a14f7ac4
nazariiko <nazarii.dev@gmail.com>
2023-11-07 23:07:24 +0200

    fix

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
+  },
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
   }
 });
 
