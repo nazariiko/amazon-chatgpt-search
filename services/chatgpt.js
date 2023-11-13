d1ec7147b0e505620bc412a40d8beeb30133a718
nazariiko <nazarii.dev@gmail.com>
2023-11-13 15:30:56 +0200

    fix

--- services/chatgpt.js
+++ services/chatgpt.js
@@ -34,7 +34,7 @@ export default class ChatGPTService {
     return new Promise(async (resolve, reject) => {
       try {
         const listProduct = products.slice(0, 17).map((product, index) => {
-          return `${index + 1}. ${product.title}, Price: ${product.price}.`
+          return `${index + 1}. ${product.title}, Price: ${product.price}.\n`
         })
         const content = `
           ${listProduct.join(' ')}
