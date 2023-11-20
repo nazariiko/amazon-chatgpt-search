6007d58dffbb6fc8e475100e9e26a0a49731425b
nazariiko <nazarii.dev@gmail.com>
2023-11-20 13:56:25 +0200

    fix

--- services/chatgpt.js
+++ services/chatgpt.js
@@ -38,7 +38,7 @@ export default class ChatGPTService {
         })
         const content = `
           ${listProduct.join(' ')}
-          "${search}". First remove superlatives and then find the exact matches. Then rank according to the superlatives. List 2 results. If there are 2 same matches, pick the cheaper one. opt for kits when available. write only the numbers on separate lines. do not write any words or sentences. prices are listed at the end, if necessary.
+          "${search}". Find the closest matches to the term. List 2 results. If there are 2 same matches, pick the cheaper one. opt for kits when available. write only the numbers on separate lines. do not write any words or sentences. prices are listed at the end, if necessary.
         `
         console.log(content);
         const completion = await this.openai.chat.completions.create({
