99aa7a091a1e9aba04c2e8176ddfd82cc14c0e80
nazariiko <nazarii.dev@gmail.com>
2023-11-28 00:53:56 +0200

    fix

--- index.js
+++ index.js
@@ -1,88 +1,94 @@
 import express from 'express';
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { createServer as createHTTP } from 'http';
 import { createServer as createHTTPS } from 'https';
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 import ChatGPTService from './services/chatgpt.js';
 import { getSearchResult, parseAmazonProducts, parseAmazonProducts2 } from './services/google.js';
 import { getProducts } from './services/amazon.js';
 
-dotenv.config()
+dotenv.config();
 
-const isProduction = process.env.IS_PRODUCTION
+const isProduction = process.env.IS_PRODUCTION;
 const app = express();
 let server;
 
 if (isProduction == 'true') {
-  server = createHTTPS({
-    key: readFileSync("/etc/letsencrypt/live/abx123.com/privkey.pem"),
-    cert: readFileSync("/etc/letsencrypt/live/abx123.com/cert.pem")
-  }, app);
+  server = createHTTPS(
+    {
+      key: readFileSync('/etc/letsencrypt/live/abx123.com/privkey.pem'),
+      cert: readFileSync('/etc/letsencrypt/live/abx123.com/cert.pem'),
+    },
+    app,
+  );
 } else {
   server = createHTTP(app);
 }
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
   },
 });
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-}));
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+  }),
+);
 
-
-const PORT = 3000 || process.env.PORT
+const PORT = 3000 || process.env.PORT;
 const chatgptServiceInstance = new ChatGPTService();
-
 
 app.get('/search', async (req, res) => {
   const socket = io.of('/search');
-  const dateStart = new Date()
+  const dateStart = new Date();
   const text = req.query.text;
   const searchTerm = await chatgptServiceInstance.getSimpleSearchTerm(text);
 
-  socket.emit('message', '1')
+  socket.emit('message', '1');
 
   let productResults;
 
   const link = await getSearchResult(searchTerm);
 
-  socket.emit('message', '2')
+  socket.emit('message', '2');
 
   productResults = await parseAmazonProducts2(link);
 
-  socket.emit('message', '3')
+  socket.emit('message', '3');
 
   // productResults = await getProducts(searchTerm)
 
-  const bestMatchesProductsIndexes = await chatgptServiceInstance.getBestMatchesProducts(text, productResults);
+  const bestMatchesProductsIndexes = await chatgptServiceInstance.getBestMatchesProducts(
+    text,
+    searchTerm,
+    productResults,
+  );
   const finalProducts = [];
-  bestMatchesProductsIndexes.forEach(index => {
-    finalProducts.push(productResults[+index - 1])
-  })
+  bestMatchesProductsIndexes.forEach((index) => {
+    finalProducts.push(productResults[+index - 1]);
+  });
 
-  const top3Products = finalProducts.slice(0, 2)
+  const top3Products = finalProducts.slice(0, 2);
 
-  const dateEnd = new Date()
+  const dateEnd = new Date();
   const time = dateEnd - dateStart;
   console.log(time / 1000);
 
   console.log(top3Products);
 
-  res.json({ status: 200, products: top3Products, error: null })
-})
+  res.json({ status: 200, products: top3Products, error: null });
+});
 
 app.get('/test', (req, res) => {
-  res.send('<h2>Working</h2>')
-})
-
+  res.send('<h2>Working</h2>');
+});
 
 io.of('/search').on('connection', (socket) => {
   console.log('A client connected to /search namespace');
@@ -92,7 +98,6 @@ io.of('/search').on('connection', (socket) => {
   });
 });
 
-
 server.listen(PORT, () => {
   console.log('Server runs');
-})
+});
--- services/chatgpt.js
+++ services/chatgpt.js
@@ -6,7 +6,7 @@ export default class ChatGPTService {
       apiKey: process.env.OPENAI_API_KEY,
     });
 
-    this.openai = openai
+    this.openai = openai;
   }
 
   getSimpleSearchTerm = (text) => {
@@ -14,86 +14,84 @@ export default class ChatGPTService {
       try {
         const completion = await this.openai.chat.completions.create({
           messages: [
-            { 
-              role: "system", 
-              content: `"${text}" if it's describing problem write only one specific product name that is a solution, no other words, if it's regarding a gift write a creative gift product idea, no other words, otherwise just write the words without superlatives.` 
-            }
+            {
+              role: 'system',
+              content: `"${text}" if it's describing problem or something that needs to be fixed only write one specific product name that is a solution, no other words, if it's regarding a gift write a creative gift product idea, no other words, otherwise only write the exact phrase again without superlatives or pricing, with only adjectives and nouns.`,
+            },
           ],
-          model: "gpt-4-1106-preview",
+          model: 'gpt-4-1106-preview',
         });
-        const result = completion.choices[0]['message']['content'].replaceAll('"', '')
+        const result = completion.choices[0]['message']['content'].replaceAll('"', '');
         console.log(result);
-        resolve(result)
+        resolve(result);
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
-  }
+    });
+  };
 
-  getBestMatchesProducts = (search, products) => {
+  getBestMatchesProducts = (search, searchTerm, products) => {
     return new Promise(async (resolve, reject) => {
       try {
         const listProduct = products.slice(0, 32).map((product, index) => {
-          return `${index + 1}. ${product.title}, Price: ${product.price}.\n`
-        })
+          return `${index + 1}. ${product.title}, Price: ${product.price}.\n`;
+        });
         const content1 = `
           ${listProduct.join(' ')}
-          which of the above are the exact matches for "${search}"? just write the listing numbers, do not write any other words.
-        `
+          which of the above are matches for "${searchTerm}"? just write the listing numbers, do not write any other words.
+        `;
         console.log(content1);
         const response1 = await this.openai.chat.completions.create({
           messages: [
-            { 
-              role: "system", 
+            {
+              role: 'system',
               content: content1,
-            }
+            },
           ],
-          model: "gpt-4-1106-preview",
+          model: 'gpt-4-1106-preview',
         });
-        const result1 = response1.choices[0]['message']['content']
+        const result1 = response1.choices[0]['message']['content'];
         console.log(result1);
 
         const content2 = `
-          from those results, which 2 are the "${search}"? only write the listing numbers on separate lines
-        `
+          which 2 of those are the "${search}"? only write the listing numbers on separate lines. do not write any words.
+        `;
 
         const response2 = await this.openai.chat.completions.create({
           messages: [
-            { 
-              role: "system", 
+            {
+              role: 'system',
               content: content1,
             },
             {
-              role: "assistant",
-              content: result1
+              role: 'assistant',
+              content: result1,
             },
             {
-              role: "user",
-              content: content2
+              role: 'user',
+              content: content2,
             },
           ],
-          model: "gpt-4-1106-preview",
+          model: 'gpt-4-1106-preview',
         });
 
-        const result2 = response2.choices[0]['message']['content']
+        const result2 = response2.choices[0]['message']['content'];
         console.log(result2);
 
-        const resultArr = result2.split('\n')
+        const resultArr = result2.split('\n');
         const newResultArr = [];
 
         for (const item of resultArr) {
           if (item !== undefined) {
-            newResultArr.push(item)
+            newResultArr.push(item);
           } else {
             break;
           }
         }
-        resolve(newResultArr)
+        resolve(newResultArr);
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
-  }
+    });
+  };
 }
-
-
