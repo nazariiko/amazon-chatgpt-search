4f765bef062ead3f016eaf09da6504c2d4dbba80
nazariiko <nazarii.dev@gmail.com>
2023-12-08 12:22:03 +0200

    f

--- services/chatgpt.js
+++ services/chatgpt.js
@@ -55,32 +55,32 @@ export default class ChatGPTService {
         const result1 = response1.choices[0]['message']['content'];
         console.log(result1);
 
-        // const content2 = `
-        //   from those results, which one is the "${search}"? only write the listing number on a separate line, no words.
-        // `;
+        const content2 = `
+          from those results, which one is the "${search}"? only write the listing number on a separate line, no words.
+        `;
 
-        // const response2 = await this.openai.chat.completions.create({
-        //   messages: [
-        //     {
-        //       role: 'system',
-        //       content: content1,
-        //     },
-        //     {
-        //       role: 'assistant',
-        //       content: result1,
-        //     },
-        //     {
-        //       role: 'user',
-        //       content: content2,
-        //     },
-        //   ],
-        //   model: 'gpt-4-1106-preview',
-        // });
+        const response2 = await this.openai.chat.completions.create({
+          messages: [
+            {
+              role: 'system',
+              content: content1,
+            },
+            {
+              role: 'assistant',
+              content: result1,
+            },
+            {
+              role: 'user',
+              content: content2,
+            },
+          ],
+          model: 'gpt-4-1106-preview',
+        });
 
-        // const result2 = response2.choices[0]['message']['content'];
-        // console.log(result2);
+        const result2 = response2.choices[0]['message']['content'];
+        console.log(result2);
 
-        const resultArr = result1.split('\n');
+        const resultArr = result2.split('\n');
         const newResultArr = [];
 
         for (const item of resultArr) {
